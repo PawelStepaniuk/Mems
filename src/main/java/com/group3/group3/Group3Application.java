350da2ebab8b8d413ba13e9a751769b3d1ef0e8c
Pawel <stepaniuk.pawel@gmail.com>
2018-11-20 12:34:00 +0100

    v0

---
+++ src/main/java/com/group3/group3/Group3Application.java
@@ -0,0 +1,12 @@
+package com.group3.group3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Group3Application.class, args);
+    }
+}
